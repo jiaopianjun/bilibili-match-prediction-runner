945e919c9bfba5d04ee247241beb1930dfe4f46f
My-Github-Repositories <77826357+My-Github-Repositories@users.noreply.github.com>
2021-08-22 01:54:01 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-if [ -d "./jar" ];then echo "./jar exist" ;fi
 version=$(wget -qO- -t1 -T2 "https://api.github.com/repos/JunzhouLiu/bilibili-match-prediction/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/V//g')
-if [ "${version}" = "$(head -n 1 './jar/jver')" ];then exit ;fi
 wget -O "bilibili-match-prediction.zip" "https://github.com/JunzhouLiu/bilibili-match-prediction/releases/download/V${version}/bilibili-match-prediction-v${version}.zip"
 mkdir "jar"
 unzip -o "bilibili-match-prediction.zip" -d "./jar/"
 mv "./jar/bilibili-match-prediction-v${version}.jar" "./jar/bilibili-match-prediction.jar" -f
 cp -f "./config.json" "./jar/config.json"
 rm "bilibili-match-prediction.zip"
-echo "${version}">"./jar/jver"
-cat "./jar/jver"
 echo "下载完成"
